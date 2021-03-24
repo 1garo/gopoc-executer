85016642dbfd8a029db717904f6a53a378ab74a4
1garo <alevardai@hotmail.com>
2021-03-24 20:20:41 -0300

    feat: rewrite queue to serve our purpose

--- handler/queue.go
+++ handler/queue.go
@@ -1,6 +1,6 @@
 package handler
 
-//TODO: use this as a helper to consume test queues 
+//TODO: use this as a helper to consume test queues
 
 import (
 	"bytes"
@@ -10,37 +10,34 @@ import (
 	"github.com/streadway/amqp"
 )
 
-func failOnError(err error, msg string) {
+func FailOnError(err error, msg string) {
 	if err != nil {
 		log.Fatalf("%s: %s", msg, err)
 	}
 }
 
-func main() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
+func Sender(q *amqp.Queue, ch *amqp.Channel) {
+	body := "Hello World!"
+	err := ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	FailOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s", body)
+}
 
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
-
-	q, err := ch.QueueDeclare(
-		"task_queue", // name
-		true,         // durable
-		false,        // delete when unused
-		false,        // exclusive
-		false,        // no-wait
-		nil,          // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
-	err = ch.Qos(
+func Consumer(q *amqp.Queue, ch *amqp.Channel) {
+	err := ch.Qos(
 		1,     // prefetch count
 		0,     // prefetch size
 		false, // global
 	)
-	failOnError(err, "Failed to set QoS")
+	FailOnError(err, "Failed to set QoS")
 
 	msgs, err := ch.Consume(
 		q.Name, // queue
@@ -51,13 +48,16 @@ func main() {
 		false,  // no-wait
 		nil,    // args
 	)
-	failOnError(err, "Failed to register a consumer")
+	FailOnError(err, "Failed to register a consumer")
 
 	forever := make(chan bool)
 
 	go func() {
 		for d := range msgs {
 			log.Printf("Received a message: %s", d.Body)
+			Process()
+			// function exection is mocked
+			// Process()
 			dotCount := bytes.Count(d.Body, []byte("."))
 			t := time.Duration(dotCount)
 			time.Sleep(t * time.Second)
